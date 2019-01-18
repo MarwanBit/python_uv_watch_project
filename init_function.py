90586d25b17bea789fbb55842966910b53d3b4ff
MarwanBit <38559602+MarwanBit@users.noreply.github.com>
2019-01-17 19:29:45 -0500

    Create init_function.py

---
+++ init_function.py
@@ -0,0 +1,22 @@
+from uv_watch_class import UVWatch
+
+def init_function():
+	loop_boolean = True 
+		if loop_boolean == True:
+			try:
+				'''
+				This code is a place holder until we can get a function to pull real data
+				'''
+				self.input_type_list =  
+				break
+				loop_boolean = False
+			except ValueError:
+				'''
+				This code will be edited once their is a function for actually pulling real data from sensors
+				'''
+				user_input_temperature = input("\ntype in a new temperature:")
+				self.temperature = user_input_temperature
+				user_input_absorbtion_wavelength_levels = input("\ntype in a new wavelength:")
+				self.absorbtion_wavelength_levels = user_input_absorbtion_wavelength_levels
+				user_input_heart_rate_bpm = input("\ntype in a new bpm:")
+				self.heart_rate_bpm = heart_rate_bpm
